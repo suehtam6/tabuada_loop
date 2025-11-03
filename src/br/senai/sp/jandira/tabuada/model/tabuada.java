2441094566c6db1791be0fe41b38242b19019a39
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2025-11-03 15:05:09 -0300

    Começando o trabalho

--- src/br/senai/sp/jandira/tabuada/model/tabuada.java
+++ src/br/senai/sp/jandira/tabuada/model/tabuada.java
@@ -1,4 +1,38 @@
 package br.senai.sp.jandira.tabuada.model;
 
+import java.util.Scanner;
+
 public class tabuada {
+
+    double multiplicando;
+    double multiplicadorInicial;
+    double multiplicadorFinal;
+
+
+      public void exibirDados() {
+
+          Scanner leitor = new Scanner(System.in);
+
+          System.out.println("======================");
+          System.out.println("Qual o número que vai ser multiplicado?");
+          multiplicando = leitor.nextDouble();
+
+          System.out.println("Qual vai ser o miltiplicador inicial?");
+          multiplicadorInicial = leitor.nextDouble();
+
+          System.out.println("Qual vai ser o miltiplicador final?");
+          multiplicadorFinal = leitor.nextDouble();
+
+
+      }
+
+
+
+
+
+    }
+
+
+
+
 }
--- src/br/senai/sp/jandira/tabuada/tabuadaApp.java
+++ src/br/senai/sp/jandira/tabuada/tabuadaApp.java
@@ -1,4 +1,6 @@
 package br.senai.sp.jandira.tabuada;
 
 public class tabuadaApp {
+    public static void main(String[] args) {
+    }
 }
